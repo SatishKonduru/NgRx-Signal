3423ebadadbaa3676e49deb334b5c6393e3f5841
satish <satishkonduru0225@gmail.com>
2024-03-04 12:00:59 +0530

    install angulat material and added ngrx

---
+++ NgRx-Signal/src/app/model/mock-data.ts
@@ -0,0 +1,14 @@
+import { Todo } from "./todo.model";
+
+export const TODOS: Todo[] = [
+    {id: '1', title: 'Angular', completed: false},
+    {id: '2', title: 'React', completed: true},
+    {id: '3', title: 'Vue', completed: false},
+    {id: '4', title: 'Bootstrap', completed: false},
+    {id: '5', title: 'Angular Material', completed: true},
+    {id: '6', title: 'NodeJS', completed: false},
+    {id: '7', title: 'MongoDB', completed: true},
+    {id: '8', title: 'ExpressJS', completed: true},
+    {id: '9', title: 'MySQL', completed: true},
+    {id: '10', title: 'TypeScript', completed: false},
+]
---
+++ NgRx-Signal/src/app/modules/angular-material/angular-material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+const materialComponents: string[] = [
+  
+]
+@NgModule({
+
+  imports: [materialComponents],
+  exports: [materialComponents]
+})
+export class AngularMaterialModule { }
